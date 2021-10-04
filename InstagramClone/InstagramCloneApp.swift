227eecd97bf900c8b719206447c8f79662356991
sans-connaissance <davemalicke@gmail.com>
2021-10-04 12:53:11 -0400

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by David Malicke on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
